981081ab487c54d27e8c6c1c9a8c16defd7a00fe
KaitlynClay <122916928+KaitlynClay@users.noreply.github.com>
2024-02-22 17:51:39 -0600

    works single button

--- alertControllerApp/ViewController.swift
+++ alertControllerApp/ViewController.swift
@@ -15,5 +15,14 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func btnTapped(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Error", message: "Your horse is a beauty!", preferredStyle: .actionSheet)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {action -> Void in
+        })
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
